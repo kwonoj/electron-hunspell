bb24dc21284106f686f288647e4735edba83e0ef
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-08 23:35:56 -0700

    feat(logger): implement logger

---
+++ src/util/logger.ts
@@ -0,0 +1,36 @@
+type logFunctionType = (message: string, ...optionalParams: Array<any>) => void;
+type logObjectType = {
+  debug: logFunctionType;
+  info: logFunctionType;
+  warn: logFunctionType;
+  error: logFunctionType;
+};
+
+/**
+ * noop fn for default log behavior.
+ */
+const noopLog = (..._args: Array<any>) => {
+  //noop
+};
+
+const log: Readonly<logObjectType> = {
+  debug: noopLog,
+  info: noopLog,
+  warn: noopLog,
+  error: noopLog
+};
+
+/**
+ * Set logger for prints out internal behavior.
+ * @param {logFunctionType | logObjectType} logger Logger object contains loglevel function (debug, info, warn, error)
+ * or single function to log. If single function is provided, all loglevel will use given function.
+ * If logger object is provided, it should have all loglevel function.
+ */
+function enableLogger(logger: logFunctionType): void;
+function enableLogger(logger: logObjectType): void;
+function enableLogger(logger: logFunctionType | logObjectType) {
+  const isLogFunction = typeof logger === 'function';
+  Object.keys(log).forEach(key => (log[key] = isLogFunction ? logger : logger[key]));
+}
+
+export { logFunctionType, logObjectType, enableLogger, log };
