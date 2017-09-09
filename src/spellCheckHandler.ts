372d3ef52db3dac9dfd7ab6248a195b96cfeadfd
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-08 23:35:05 -0700

    refactor(spellcheckerprovider): rename main provider

--- src/spellCheckHandler.ts
+++
@@ -1,9 +0,0 @@
-interface SpellCheckHandlerBase {}
-
-class SpellCheckHandler implements SpellCheckHandlerBase {
-  public attach() {
-    throw new Error('not implemented');
-  }
-}
-
-export { SpellCheckHandlerBase, SpellCheckHandler };
