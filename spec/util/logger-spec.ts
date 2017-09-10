68fdd74565abb7af31437b8974e1fb7c5a54b715
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-09 22:32:37 -0700

    test(logger): update test cases

--- spec/util/logger-spec.ts
+++ spec/util/logger-spec.ts
@@ -6,23 +6,29 @@ describe('logger', () => {
   let log: typeof loggerType.log;
   let enableLogger: typeof loggerType.enableLogger;
 
-  beforeEach(() => ({ log, enableLogger } = require('../../src/util/logger')));
+  beforeEach(() => {
+    jest.mock('hunspell-asm');
+
+    ({ log, enableLogger } = require('../../src/util/logger'));
+  });
 
   it('should do nothing by default', () => {
     Object.keys(log).forEach(logLevel => expect(() => log[logLevel]('')).to.not.throw());
   });
 
   it('should accept log function', () => {
-    const mock = jest.fn();
-    enableLogger(mock);
+    const mockLogFn = jest.fn();
+    enableLogger(mockLogFn);
 
     const message = 'message';
     const value = { value: 'value' };
 
     Object.keys(log).forEach(logLevel => log[logLevel](message, value));
 
-    expect(mock.mock.calls).to.have.lengthOf(4);
-    expect(mock.mock.calls).to.deep.equal([[message, value], [message, value], [message, value], [message, value]]);
+    const mockCalls = mockLogFn.mock.calls;
+
+    expect(mockCalls).to.have.lengthOf(4);
+    expect(mockCalls).to.deep.equal([[message, value], [message, value], [message, value], [message, value]]);
   });
 
   it('should accept log object', () => {
@@ -41,8 +47,33 @@ describe('logger', () => {
     Object.keys(log).forEach(logLevel => log[logLevel](message, value));
 
     Object.keys(mockLogger).forEach(logLevel => {
-      expect(mockLogger[logLevel].mock.calls).to.have.lengthOf(1);
-      expect(mockLogger[logLevel].mock.calls[0]).to.deep.equal([message, value]);
+      const mockCalls = mockLogger[logLevel].mock.calls;
+      expect(mockCalls).to.have.lengthOf(1);
+      expect(mockCalls[0]).to.deep.equal([message, value]);
     });
+  });
+
+  it('should set function to hunspell logger', () => {
+    const hunspellEnableLogger = require('hunspell-asm').enableLogger;
+
+    const mockLogFn = jest.fn();
+    enableLogger(mockLogFn);
+
+    expect(hunspellEnableLogger.mock.calls).to.have.lengthOf(1);
+  });
+
+  it('should set debug fn from object to hunspell logger', () => {
+    const hunspellEnableLogger = require('hunspell-asm').enableLogger;
+
+    const mockLogger = {
+      debug: jest.fn(),
+      info: jest.fn(),
+      warn: jest.fn(),
+      error: jest.fn()
+    };
+
+    enableLogger(mockLogger);
+
+    expect(hunspellEnableLogger.mock.calls).to.have.lengthOf(1);
   });
 });
