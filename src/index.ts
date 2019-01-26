2c221d4049b7e4a4ac4ccabcd0e7f7a33fc8a767
OJ Kwon <kwon.ohjoong@gmail.com>
2019-01-25 21:53:34 -0800

    feat(createprovider): treeshakable provider factory
    BREAKING CHANGE: provider create moved to factory function
    BREAKING CHANGE: wasm binary is no longer bundled in js preamble

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { SpellCheckerProvider } from './spellCheckerProvider';
 export { enableLogger } from './util/logger';
+export { createProvider } from './createProvider';
+export { createAsmProvider } from './createAsmProvider';
