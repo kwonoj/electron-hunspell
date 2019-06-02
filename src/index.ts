a274423c73b08135498806d7ff7e2755ddbe58f0
OJ Kwon <kwon.ohjoong@gmail.com>
2019-06-01 18:51:11 -0700

    feat(export): export environment enum

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { SpellCheckerProvider } from './spellCheckerProvider';
 export { enableLogger } from './util/logger';
 export { ProviderProxy, attachSpellCheckProvider } from './attachSpellCheckProvider';
+export { ENVIRONMENT } from 'emscripten-wasm-loader';
