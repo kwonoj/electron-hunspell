da8f87c8ca5d2bc155c172493346f43cc645013c
OJ Kwon <kwon.ohjoong@gmail.com>
2019-06-12 23:48:39 -0700

    feat(spellcheckerprovider): do not accept environment override
    BREAKING CHANGES: do not allow environment override

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { SpellCheckerProvider } from './spellCheckerProvider';
 export { enableLogger } from './util/logger';
 export { ProviderProxy, attachSpellCheckProvider } from './attachSpellCheckProvider';
-export { ENVIRONMENT } from 'emscripten-wasm-loader';
